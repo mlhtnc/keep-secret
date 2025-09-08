b5388413824e4008e61d91489befa776e60097c9
Melih <mlhtncc@gmail.com>
2025-09-08 12:30:04 +0300

    Fix flickering issue on LoginScreen
    It was because of SafeAreaView, recalculating safe areas were causing flickering issue on first renfer

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import BootSplash from "react-native-bootsplash";
 
 import LoginScreen from './src/screens/LoginScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import DetailsScreen from './src/screens/DetailsScreen';
-import { Colors, DetailsScreenName, HomeScreenName, LoginScreenName } from './src/constants';
+import { DetailsScreenName, HomeScreenName, LoginScreenName } from './src/constants';
 import { RootStackParamList } from './src/types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -14,16 +15,14 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: Colors.background }} >
-        <StatusBar translucent backgroundColor={"transparent"} barStyle="light-content" />
-          <NavigationContainer>
-            <Stack.Navigator initialRouteName={LoginScreenName} screenOptions={{ headerShown: false, animation: 'none' }}>
-              <Stack.Screen name={LoginScreenName} component={LoginScreen} />
-              <Stack.Screen name={HomeScreenName} component={HomeScreen} />
-              <Stack.Screen name={DetailsScreenName} component={DetailsScreen} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </SafeAreaView>
+      <StatusBar translucent backgroundColor={"transparent"} barStyle="light-content" />
+        <NavigationContainer onReady={() => BootSplash.hide() }>
+          <Stack.Navigator initialRouteName={LoginScreenName} screenOptions={{ headerShown: false, animation: 'none' }}>
+            <Stack.Screen name={LoginScreenName} component={LoginScreen} />
+            <Stack.Screen name={HomeScreenName} component={HomeScreen} />
+            <Stack.Screen name={DetailsScreenName} component={DetailsScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
     </SafeAreaProvider>
   );
 }
